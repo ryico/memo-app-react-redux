2805b5e3bc5c34fe2a132287dcef15a558f18a69
Ichiki Ryo <tohodaiichiki@gmail.com>
2020-08-09 02:10:50 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+import Memo from './memo/Memo';
+import AddForm from './memo/AddForm';
+import FindForm from './memo/FindForm';
+import DeleteForm from './memo/DeleteForm';
+
+class App extends Component {
+  td = {
+    width: '250px'
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Memo App that uses React and Redux</h1>
+        <AddForm />
+        <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td style={this.td}><FindForm /></td>
+              <td style={this.td}><DeleteForm /></td>
+            </tr>
+          </tbody>
+        </table>
+        <Memo />
+      </div>
+    );
+  }
+}
+
+export default connect()(App);
