f144b1ceb6b5d73f4e4546b607c18362fb51c24c
Ichiki Ryo <tohodaiichiki@gmail.com>
2020-08-09 02:16:24 +0900

    add redux and react-redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import MemoStore from './memo/Store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
