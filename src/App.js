fe5124043f46b07558be4e668adef083a2d97122
Ichiki Ryo <tohodaiichiki@gmail.com>
2020-08-10 21:31:57 +0900

    add switch persist checkbox

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Memo from './memo/Memo';
 import AddForm from './memo/AddForm';
 import FindForm from './memo/FindForm';
 import DeleteForm from './memo/DeleteForm';
+import PersistForm from './memo/PersistForm';
 
 class App extends Component {
   td = {
@@ -18,14 +19,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Memo App that uses React and Redux</h1>
+        <h1> Memo App that uses React and Redux </h1>
         <AddForm />
-        <hr />
+        <hr/>
         <table>
           <tbody>
             <tr>
-              <td style={this.td}><FindForm /></td>
-              <td style={this.td}><DeleteForm /></td>
+              <td style={this.td}>
+                <FindForm />
+              </td>
+              <td style={this.td}>
+                <DeleteForm />
+              </td>
+              <td style={this.td}>
+                <PersistForm />
+              </td>
             </tr>
           </tbody>
         </table>
